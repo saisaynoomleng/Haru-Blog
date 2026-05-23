01892583586ee2cdb2603eb00ef38ef726e4c07d
saisaynoomleng <saileng9723@gmail.com>
2026-05-23 15:27:54 -0400

    finish paper torn

---
+++ src/components/shared/PaperTorn.tsx
@@ -0,0 +1,18 @@
+import clsx from 'clsx';
+import { twMerge } from 'tailwind-merge';
+
+type PaperTornProps = {
+  className?: string;
+  children: React.ReactNode;
+};
+
+const MASK_CLASS =
+  '[mask-image:url(/video-mask.png)] [mask-mode:alpha] [mask-position:center] [mask-repeat:no-repeat] [mask-size:100%_100%]';
+
+export const PaperTorn = ({ className, children }: PaperTornProps) => {
+  return (
+    <div className={twMerge(clsx('relative w-full h-full', className))}>
+      <div className={clsx(MASK_CLASS, 'w-full h-full')}>{children}</div>
+    </div>
+  );
+};
