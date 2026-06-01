f40501a5684134bdc8e9683b03173599ef8f5d62
saisaynoomleng <saileng9723@gmail.com>
2026-05-31 20:16:04 -0400

    finish sign in page

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -20,6 +20,8 @@ const buttonVariants = cva(
         destructive:
           'bg-destructive/10 text-destructive hover:bg-destructive/20 focus-visible:border-destructive/40 focus-visible:ring-destructive/20 dark:bg-destructive/20 dark:hover:bg-destructive/30 dark:focus-visible:ring-destructive/40',
         link: 'text-primary underline-offset-4 hover:underline',
+        oAuth:
+          'bg-transparent border border-brand-neutral-950 rounded-lg hover:bg-brand-primary-300',
       },
       size: {
         default:
